06dc65b990c4d8f1d1205fa3b76e919ad170ab47
lifan06 <lifan06@kuaishou.com>
2024-04-03 14:02:03 +0800

    feat:

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,21 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { sum, contactStr, getOptions } from '../index.js'
 
 test('sum from native', (t) => {
   t.is(sum(1, 2), 3)
 })
+
+test('contact_str from native', (t) => {
+    t.is(contactStr('a', 'b'), 'ab');
+})
+
+test("getOptions from native", (t) => {
+    const options = {
+      id: 1,
+      name: "napi-rs",
+    };
+    console.log('11', getOptions(options));
+    console.log('22', options);
+});
+
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,26 @@ extern crate napi_derive;
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
 }
+
+#[napi]
+pub fn contact_str(a: String, b: String) -> String {
+    format!("{}{}", a, b)
+}
+
+#[napi(object)]
+pub struct ToolOptions {
+    pub id: i32,
+    pub name: String,
+}
+
+#[napi]
+pub fn get_options(options: ToolOptions) -> ToolOptions {
+    println!("id:{}, name:{}", options.id, options.name);
+    options
+}
+
+use napi::{Task, Env, Result, JsNumber}
+
+struct AsyncFib {
+    input: u32,
+}
